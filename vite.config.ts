f5ac1e16e48a0d6c4232c4f2149c117d03d8a28e
pradhan1995 <dramitpradhan1995@gmail.com>
2026-04-26 04:17:46 +0530

    fix tsconfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
+/// <reference types="vite/client" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: import.meta.env.VITE_BASE_PATH || "/react-vite-deploy",
   server: {
     port: 3000,
   },
